835c32a9bd93dd5fd66d610189eed73e0ec62999
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-10-30 13:08:54 -0300

    clean example

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/AddNote.kt
@@ -0,0 +1,21 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.InvalidNoteException
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class AddNote(
+    private val repository: NoteRepository
+) {
+
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note) {
+        if(note.title.isBlank()) {
+            throw InvalidNoteException("O título da nota é obrigatório.")
+        }
+        if(note.content.isBlank()) {
+            throw InvalidNoteException("O conteúdo da nota é obrigatório.")
+        }
+        repository.insertNote(note)
+    }
+}
